3042271be63da57e42405fab1969b90518ec0262
S41N7 <s41n7@pop-os.localdomain>
2022-08-31 15:25:25 +0530

    added swipe to delete tasks and undo

---
+++ app/src/main/java/com/codinginflow/mvvmtodo/data/TaskDatabase.kt
@@ -0,0 +1,37 @@
+package com.codinginflow.mvvmtodo.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.codinginflow.mvvmtodo.di.ApplicationScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Provider
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+
+    class Callback @Inject constructor(
+        private val database: Provider<TaskDatabase>,
+        @ApplicationScope private val applicationScope: CoroutineScope
+
+    ) : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+
+            val dao = database.get().taskDao()
+            applicationScope.launch {
+                dao.insert(Task("Wash the dishes", important = true))
+                dao.insert(Task("Do the laundry", completed = true))
+                dao.insert(Task("Buy Groceries"))
+                dao.insert(Task("Prepare food", completed = true))
+                dao.insert(Task("Call mom", important = true))
+                dao.insert(Task("Call Musk"))
+                dao.insert(Task("Wash the dishes"))
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TaskFragment.kt
@@ -0,0 +1,123 @@
+package com.codinginflow.mvvmtodo.ui.tasks
+
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codinginflow.mvvmtodo.R
+import com.codinginflow.mvvmtodo.data.SortOrder
+import com.codinginflow.mvvmtodo.data.Task
+import com.codinginflow.mvvmtodo.databinding.FragmentTaskBinding
+import com.codinginflow.mvvmtodo.util.onQueryTextChanged
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class TaskFragment : Fragment(R.layout.fragment_task), TasksAdapter.OnItemClickListener {
+
+
+    private val viewModel: TaskViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTaskBinding.bind(view)
+        val tasksAdapter = TasksAdapter(this)
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = tasksAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val task = tasksAdapter.currentList[viewHolder.adapterPosition]
+                viewModel.onTaskSwiped(task)
+            }
+        }).attachToRecyclerView(binding.recyclerViewTasks)
+
+        viewModel.tasks.observe(viewLifecycleOwner) {
+            tasksAdapter.submitList(it)
+        }
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.taskEvent.collect { event ->
+                when (event) {
+                    is TaskViewModel.TasksEvent.ShowUndoDeleteTaskMessage -> {
+                        Snackbar.make(requireView(), "Task deleted", Snackbar.LENGTH_LONG)
+                            .setAction("UNDO") {
+                                viewModel.onUndoDeleteClick(event.task)
+                            }.show()
+                    }
+                }
+
+            }
+        }
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_task, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            viewModel.searchQuery.value = it
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            menu.findItem(R.id.action_hide_completed_task).isChecked =
+                viewModel.preferencesFlow.first().hideCompleted
+        }
+    }
+
+    override fun onItemClick(task: Task) {
+        viewModel.onTaskSelected(task)
+    }
+
+    override fun onCheckBoXClick(task: Task, isChecked: Boolean) {
+        viewModel.onTaskCheckedChanged(task, isChecked)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_sort_by_name -> {
+                viewModel.onSortOrderSelected(SortOrder.BY_NAME)
+                true
+            }
+            R.id.action_sort_by_date -> {
+                viewModel.onSortOrderSelected(SortOrder.BY_DATE)
+                true
+            }
+            R.id.action_hide_completed_task -> {
+                item.isChecked = !item.isChecked
+                viewModel.onHideCompleted(item.isChecked)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
